9345bac386fc1d1de1c55c7b3712157272fe114c
alex.apollonina <alex.apollonina@gmail.com>
2024-02-16 16:23:36 +0200

    Added actions and reducers

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,27 +1,31 @@
-import React, {useState, useEffect} from 'react';
+import React, {Component} from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import './App.css';
 
-
-
-function App () {
-
-    const [robots, setRobots] = useState([]);
-    const [searchfield, setSearchfield] = useState('');
-
-    useEffect(() => {
-         fetch('https://jsonplaceholder.typicode.com/users')
-        .then(response => response.json())
-        .then(users => setRobots(users));
-    }, [])
-
-    const onSearchChange = (event) => {
-        setSearchfield(event.target.value);
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            robots: [],
+            searchfield: ''
+        }
     }
- 
-    const filteredRobots = robots.filter(robot =>{
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => this.setState({robots: users}));
+        
+    }
+
+    onSearchChange = (event) => {
+        this.setState({searchfield: event.target.value});
+    }
+    render() {
+        const { robots, searchfield } = this.state;
+        const filteredRobots = robots.filter(robot =>{
             return robot.name.toLowerCase().includes(searchfield.toLowerCase());
         });
         return !robots.length ?
@@ -29,12 +33,13 @@ function App () {
                 (
                 <div className='tc'>
                     <h1 className='f1'>Robofriends</h1>
-                    <SearchBox searchChange={onSearchChange}/>
+                    <SearchBox searchChange={this.onSearchChange}/>
                     <Scroll>
                         <CardList robots={filteredRobots} />
                     </Scroll>
                 </div>
             );
+        }
     }
 
 
